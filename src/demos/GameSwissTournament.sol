f1f818670a230413f7c7fd86264cf539a2471291
saucepoint <saucepoint@protonmail.com>
2022-08-24 18:42:45 -0400

    some documentation

--- src/demos/GameSwissTournament.sol
+++ src/demos/GameSwissTournament.sol
@@ -17,13 +17,20 @@ contract GameSwissTournament is SwissTournament {
     }
 
     /// @dev take note of the advancePlayers modifier
+    ///      the modifier will update the swiss tournament logic for you
     function playMatch(ResultCounter calldata group, uint256 matchIndex) public override advancePlayers(group, matchIndex) {
+        // modifier will validate that the match has not yet been played
         Match storage matchup = matches[group.wins][group.losses][matchIndex];
+        
+        // the game logic just needs to return the id (uint256) of the winner
+        // the game logic can accept an arbitrary amount of parameters
+        // however given 2 players, return the id of the winner!
         uint256 winnerId = game.result(matchup.player0, matchup.player1);
 
-        // Implementing playMatch() requires you to update the outcome of the match
+        /// @dev Implementing playMatch() requires you to update the outcome of the match
         matchup.winnerId = winnerId;
         matchup.played = true;
+        
         // the advancePlayers() modifier will handle advancing the players to the next group
     }
 }
