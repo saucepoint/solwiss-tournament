4464e6372cd797d042b110daf2b44535f0cb1214
saucepoint <saucepoint@protonmail.com>
2022-08-24 18:43:45 -0400

    move the demo to tests actually

--- test/mocks/GameSwissTournament.sol
+++ test/mocks/GameSwissTournament.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "../SwissTournament.sol";
+import "../../src/SwissTournament.sol";
 import "./Game.sol";
 
 contract GameSwissTournament is SwissTournament {
